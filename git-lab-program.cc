e60eef89bab8ae4644b1f9efcab99c112a4b6459
Jude Pullen <jp695323@ohio.edu>
2023-09-08 17:49:56 -0400

    CS2400

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*   File: git-lab-program.cc
+*   Author: Jude Pullen
+*   Date: 9/8/2023
+*   Description: A test file for my second CS 2400 Lab class
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int arg, char const *argv[]){
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
